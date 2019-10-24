a32393b24c97a5f9edd9c7e1d656512a29a2cf0d
Jurian <j.vink@gdksoftware.nl>
2019-10-24 13:48:48 +0200

    fix

--- postcode-api/config.js
+++ postcode-api/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 4000,
-    HOST: process.env.HOST || "localhost",
+    HOST: process.env.HOST || "postcode_db",
     USER: process.env.USER || "root",
     PASSWORD: process.env.PASSWORD || "root",
     DATABASE: process.env.DATABASE || "postcodeapi",
